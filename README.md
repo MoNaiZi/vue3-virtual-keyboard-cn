81237b2322732a11791189849614a3d74e4b0e73
MoNaiZi <358665514@qq.com>
2025-12-08 10:01:58 +0800

    init

--- README.md
+++ README.md
@@ -1,5 +1,93 @@
-# Vue 3 + Vite
+# vue-virtual-keyboard
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+演示(词库较大请耐心等待) https://monaizi.github.io/vue-virtual-keyboard/
 
-Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+## 安装
+
+```bash
+yarn add vue3-virtual-keyboard-cn
+
+npm install vue3-virtual-keyboard-cn
+
+```
+
+## 使用
+
+### 局部使用
+
+```
+import keyboard from "vue3-virtual-keyboard-cn/keyboardIndex.vue"
+```
+
+### 全局注册
+
+```
+import keyboard from "vue3-virtual-keyboard-cn"
+Vue.use(keyboard) 然后使用 <vue-virtual-keyboard-cn></vue-virtual-keyboard-cn>;
+```
+
+<br/>
+
+### 要想让键盘自动绑定上，input 或 textarea 必须设置 keyboard="true"属性
+
+data-mode 默认不传是是中文，不传词库默认就是小写英文
+
+### input 属性
+
+| 属性      | 说明     | 值       | 备注               |
+| --------- | -------- | -------- | ------------------ |
+| data-mode | 键盘类型 | cn       | 中文               |
+| data-mode | 键盘类型 | en_cap   | 英文大写           |
+| data-mode | 键盘类型 | en       | 英文小写           |
+| data-mode | 键盘类型 | num      | 数字键盘           |
+| data-mode | 键盘类型 | biaodian | 符号键盘           |
+| data-mode | 键盘类型 | password | 密码键盘           |
+| data-mode | 键盘类型 | id_card  | 身份证键盘         |
+| data-mode | 键盘类型 | di_git   | 带小数点的数字键盘 |
+
+### 词库在本项目的 src/dict 目录下
+
+### 引用词库示例
+
+比如我有个词库在 src/dict 目录下：
+`正确的引用<keyboard :singleDict="'dict/baseDict.json"'"></keyboard> `
+
+!!! 注意不要把多词汇词库传递给单词汇词库的属性
+<br/>
+
+### 词库介绍
+
+| 属性          | 说明       | 备注                                  |
+| ------------- | ---------- | ------------------------------------- |
+| baseDict.json | 单词汇词库 | 基础词库                              |
+| address.json  | 多词汇词库 | 全国省市区地址(主要)和一些短词句词库  |
+| chowder.json  | 多词汇词库 | 日常生活,成语,人名,全国省市区地址词库 |
+
+<br/>
+
+### 组件属性
+
+| 属性           | 说明                     | 值            | 备注                           |
+| -------------- | ------------------------ | ------------- | ------------------------------ |
+| singleDict     | 单词汇词库               | baseDict.json | 要想使用汉字拼写功能此项必填   |
+| manyDict       | 多词汇词库               | 请看词库介绍  | 可以根据不同场景使用不同的词库 |
+| maxQuantify    | 显示选词个数             | Number        | 默认 10                        |
+| showKeyboard   | 手动显示隐藏键盘         | true/false    | 默认不需要自己手动传值         |
+| inputEvent     | 手动传入可编辑的对象     | element       | 用来处理可编辑元素             |
+| blurHide       | 失去焦点是否自动隐藏键盘 | true/false    |
+| transitionTime | 键盘动画过渡时间         | String        | 默认 '0.3s'                    |
+
+<br/>
+
+### 组件事件
+
+| 事件                 | 说明                       | 值           | 备注                                                                                        |
+| -------------------- | -------------------------- | ------------ | ------------------------------------------------------------------------------------------- |
+| clickNumber          | 点击数字符号触发           | key          |
+| clickKey             | 点击字母空格触发           | key          |
+| del                  | 点击删除触发               | key          | 如果是中文，会返回当前的拼音                                                                |
+| changeMode           | 键盘类型改变时触发         | data-mode    |
+| changeShow           | 键盘显示和隐藏触发         | true/false   |
+| initResult           | 词库加载事件               | success/fail | 成功和错误都会返回对应的结果                                                                |
+| inputBindKeyboard    | 重新给 input 绑定键盘      |              | 使用：调用组件里面的 inputBindKeyboard 方法，例子：this.$refs.keyboard.inputBindKeyboard(); |
+| contenteditableInput | 更改可编辑元素是触发的事件 | value        |
---
+++ src/App.vue
@@ -0,0 +1,248 @@
+<template>
+  <div id="app">
+    <el-input
+      v-model="value2"
+      placeholder="请输入或选择"
+      keyboard="true"
+      data-mode="cn"
+      @focus="focusInput"
+    ></el-input>
+    <el-select
+      v-model="value2"
+      ref="select"
+      placeholder="请选择"
+      class="abcSelect"
+    >
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
+    <el-select v-model="value2" ref="select" placeholder="请选择2">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
+    <div>
+      <div>
+        中文：<input
+          id="abc"
+          style="width: 400px; height: 25px; font-size: 20px"
+          type="text"
+          v-model="value"
+          keyboard="true"
+          @click="stopDefault"
+        />
+      </div>
+      <div>
+        英文大写：<input type="text" data-mode="en_cap" keyboard="true" />
+      </div>
+      <div>英文小写：<input type="text" data-mode="en" keyboard="true" /></div>
+      <div>数字：<input type="text" data-mode="num" keyboard="true" /></div>
+      <div>
+        身份证：<input type="text" data-mode="id_card" keyboard="true" />
+      </div>
+      <div>浮点：<input type="text" data-mode="di_git" keyboard="true" /></div>
+      <div>
+        符号：<input type="text" data-mode="biaodian" keyboard="true" />
+      </div>
+      <div>
+        密码键盘：<input type="text" data-mode="password" keyboard="true" />
+      </div>
+
+      自动绑定到textarea<textarea type="text" keyboard="true"></textarea>
+      <div>
+        手动显示输入法<textarea
+          type="text"
+          @focus="focusInput2"
+          @blur="blurInput2"
+        ></textarea>
+      </div>
+      <div>
+        <p contenteditable id="elem" @click="clickDiv" @blur="blurInput2">
+          div手动显示输入法： 我是可以被编辑的div
+        </p>
+      </div>
+      <div>不需要输入法<input type="text" /></div>
+      <div style="height: 400px"></div>
+      <!-- <vue-virtual-keyboard-cn
+        :transitionTime="'0.3s'"
+        :maxQuantify="10"
+        :showKeyboard="showKeyboard"
+        @clickKey="clickKey"
+        float
+        :inputEvent="currentInput"
+        @initResult="initFulfil"
+        :manyDict="manyDict"
+        :singleDict="singleDict"
+        @keyboardTips="keyboardTips"
+        @clickNumber="clickNumber"
+        :blurHide="false"
+      ></vue-virtual-keyboard-cn> -->
+      <keyboard
+        :transitionTime="'0.5s'"
+        :maxQuantify="10"
+        :showKeyboard="showKeyboard"
+        @clickKey="clickKey"
+        float
+        :inputEvent="currentInput"
+        @initResult="initFulfil"
+        :manyDict="manyDict"
+        :singleDict="singleDict"
+        @keyboardTips="keyboardTips"
+        @clickNumber="clickNumber"
+        :blurHide="false"
+      ></keyboard>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+<script>
+import keyboard from "./components/keyboard/keyboardIndex.vue";
+// import keyboard from "vue-virtual-keyboard-cn/keyboardIndex.vue";
+export default {
+  name: "App",
+  components: {
+    keyboard,
+  },
+  methods: {
+    clickNumber(key) {
+      console.log("clickNumber", key);
+    },
+    keyboardTips(txt) {
+      console.log("tips", txt);
+    },
+    initFulfil() {
+      this.loading.close();
+    },
+    clickDiv(e) {
+      console.log("点击", e.target.getAttribute("contenteditable"));
+      e.mode = "en_cap";
+      this.currentInput = e;
+      this.showKeyboard = true;
+    },
+    focusInput2(e) {
+      console.log("聚焦", e.target.getAttribute("contenteditable"));
+      e.mode = "en_cap";
+      this.currentInput = e;
+      this.showKeyboard = true;
+    },
+    blurInput2() {
+      // console.log("失焦", e);
+      this.currentInput = null;
+      this.showKeyboard = false;
+    },
+    stopDefault() {},
+    clickKey(key) {
+      console.log("key", key);
+    },
+    bodyStopMousedown(e) {
+      // debugger;
+      // e.stopPropagation();
+      e.stopImmediatePropagation();
+    },
+    cancelOrRecoveryBodyMousedown(typeText) {
+      const body = document.querySelector("body");
+      let options = { capture: false };
+      if (typeText === "cancel") {
+        body.addEventListener("mousedown", this.bodyStopMousedown, options);
+      } else {
+        body.removeEventListener("mousedown", this.bodyStopMousedown, options);
+      }
+    },
+    focusInput() {
+      setTimeout(() => {
+        this.select.visible = true;
+      }, 300);
+    },
+  },
+  mounted() {
+    if (window.screen.width < 700) {
+      // 当前设备是移动设备
+      const inputAll = document.querySelectorAll("[keyboard='true']");
+      for (let item of inputAll) {
+        item.setAttribute("inputmode", "none");
+      }
+    }
+
+    this.loading = this.$loading({
+      lock: true,
+      text: "词库加载中。。",
+      spinner: "el-icon-loading",
+      background: "rgba(0, 0, 0, 0.7)",
+    });
+    let elem = document.querySelector("#elem");
+    let observer = new MutationObserver((mutationRecords) => {
+      console.log(mutationRecords); // console.log(the changes)
+    });
+
+    // 观察除了特性之外的所有变动
+    observer.observe(elem, {
+      childList: true, // 观察直接子节点
+      subtree: true, // 及其更低的后代节点
+      characterDataOldValue: true, // 将旧的数据传递给回调
+    });
+
+    this.cancelOrRecoveryBodyMousedown("cancel");
+    this.select = this.$refs.select;
+  },
+  data() {
+    return {
+      // manyDict: "../../dict/chowder.json",
+      singleDict: "../../dict/baseDict.json",
+      currentInput: "",
+      showKeyboard: false,
+      value: "",
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕",
+        },
+        {
+          value: "选项2",
+          label: "双皮奶",
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎",
+        },
+        {
+          value: "选项4",
+          label: "龙须面",
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭",
+        },
+      ],
+      value2: "",
+    };
+  },
+};
+</script>
+
+<style >
+.keyDown {
+  background: #2c3e50;
+}
+body {
+  margin: 0px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
